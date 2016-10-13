8fbf70ef63fc67e226be3ae6908d4b75f5db3095
Johannes P <joshadow@gmail.com>
2016-10-13 00:15:50 -0700

    added manipulator example

--- test.cpp
+++ test.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <iomanip>
 #include "PrefixLog.h"
 
 using namespace std;
 using namespace Debug;
 
 struct Foo { 
-  Foo(string s) : text("Foo contains: '" + s + "'") {}
+  Foo(string s) : text("Foo contains '" + s + "'") {}
   string text;
 };
-ostream &operator<<(ostream& os, const Foo& foo) { return os << "It's a Foo!"; }
+ostream &operator<<(ostream& os, const Foo& foo) { return os << foo.text; }
 
 int main()
 {
@@ -17,16 +18,16 @@ int main()
 
   cout << "DEMO 2:\n";
   Log("|***|") << " It has "<< "insertion operator" << " support" 
-               << endl << " which works with std::endl.";
+    << endl << " which works with std::endl.";
   
   cout << "DEMO 3:\n";
   Log(5, '_') << " You don't need \\n or std::endl as the last character\n"; 
   Log(5, '_') << " But it handles them in case you forget :)" << endl;
 
   cout << "DEMO 4:\n";
-  Log("  INFO: ") << "Here is a number :" << 1.5 << endl
-                  << "And an object that overloads std::cout :\n   " 
-                  << Foo("secret");
+  Log("  INFO: ") 
+    << "It works with manipulators: " << fixed << setprecision(4) << 1.5 << endl
+    << "And std::cout overloaded objects: " << Foo("secret");
 }
 
 
