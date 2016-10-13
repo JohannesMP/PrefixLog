069e0b86e856b9a87382cc09503e41732f93b315
Johannes <joshadow@gmail.com>
2016-10-13 01:14:38 -0700

    Updated License

--- PrefixLog.h
+++ PrefixLog.h
@@ -8,15 +8,16 @@
  * This is free software. I will never charge you to use, license, or obtain 
  * this software. Doing so would make me a jerk.
  *
- * You may use this code (and by "code" I mean *anything* contained within 
- * in this project) for whatever you want. Personal use, Educational use, 
+ * You may use this code (and by "code" I mean _anything_ contained within 
+ * this project) for whatever you want. Personal use, Educational use, 
  * Corporate use, Military use, and all other uses are OK!
  *
- * I offer no warranty on anything, ever. I've tried to ensure that there are 
- * no gaping security holes where using this software might automatically send
- * your credit card information to aliens or erase your entire hard drive, but
- * it might happen. I'm sorry. However, I warned you, so you can't sue me.
- * Suing people over free software would make you a jerk.
+ * I offer no warranty on anything in this project, and you are using it of 
+ * your own free will at your own risk. I've tried my best to ensure that 
+ * there are no gaping security holes where using this software might erase
+ * your  hard drive or give your dog diarrhea, but it might happen. I'm sorry. 
+ * However, I warned you, so you can't sue me. Suing people over free software
+ * would make you a jerk.
  *
  * If you find bugs, it would be nice if you let me know so I can fix them.
  * You don't have to, but it would be appreciated.
@@ -25,13 +26,13 @@
  * to add any features for you, though I will consider your suggestions. 
  * Feeling entitled about free software would make you a jerk.
  *
- * If you add a new feature or fix a bug, it would be nice if you contributed it
- * back to the project, but you don't have to. The repository/site you obtained
- * this software from should contain a way for you to contact me. 
+ * If you add a new feature or fix a bug, it would be nice if you contributed 
+ * it back to the project, but you don't have to. The repository/site you 
+ * obtained this software from should contain a way for you to contact me. 
  * Contributing to open source makes you awesome!
  *
- * If you use this software, you may delete this license, and don't have to give
- * me any credit, but it would be great if you did.
+ * If you use this software, you may remove this license, and don't have to
+ * give me any credit, but it would be great if you did.
  *
  * Don't be a jerk. Enjoy your free software! :)
 */
